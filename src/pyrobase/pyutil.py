76bda34cf02abfaa932dcdc844bc970b1fc94504
pyroscope <pyroscope.project@gmail.com>
2012-02-02 05:36:44 +0100

    Added require_json() to load the best available json library on demand

--- src/pyrobase/pyutil.py
+++ src/pyrobase/pyutil.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=I0011
 """ Python Language / Runtime Support.
 
     Copyright (c) 2011 The PyroScope Project <pyroscope.project@gmail.com>
@@ -15,3 +17,22 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+import logging
+
+LOG = logging.getLogger(__name__)
+
+
+def require_json():
+    """ Load the best available json library on demand.
+    """
+    # Fails when "json" is missing and "simplejson" is not installed either
+    try:
+        import json # pylint: disable=F0401
+        return json
+    except ImportError:
+        try:
+            import simplejson # pylint: disable=F0401
+            return simplejson
+        except ImportError, exc:
+            raise ImportError("""Please 'easy_install "simplejson>=2.1.6"' (%s)""" % (exc,))
+
