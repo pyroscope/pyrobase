9fabab7e8ee1e1412b5893dbf672a9e22672dc7e
pyroscope <pyroscope.project@gmail.com>
2012-03-10 11:02:22 +0100

    Added imgur webservice

---
+++ src/pyrobase/io/http.py
@@ -0,0 +1,132 @@
+""" HTTP support.
+
+    Copyright (c) 2012 The PyroScope Project <pyroscope.project@gmail.com>
+"""
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import os
+import sys
+import time
+import httplib
+import urlparse
+import StringIO
+import mimetypes
+
+from pyrobase import fmt, parts
+
+
+class HttpPost(object):
+    """ Do a HTTP multipart/form-data POST.
+    """
+
+    def __init__(self, url, fields, headers=None, mock_http=False):
+        """ Initialize POST data. 
+            
+            Field values can be strings or files; files
+            are expected to have a read() method and SHOULD have a 'name'
+            attribute (i.e. look like handles returned by 'open()').
+        
+            @param url: the URL to POST to.
+            @param fields: sequence of (name, value) tuples.
+            @param headers: dict of additional headers.
+        """
+        self.url = url
+        self.fields = fields
+        self.headers = headers or {}
+        self.mock_http = mock_http
+
+
+    def __repr__(self):
+        """ Show POST data.
+        """
+        # TODO: headers
+        return '\n'.join([
+            "POST to '%s' with these fields:" % (self.url,),
+        ] + [
+            "  %s=%r" % i for i in self.fields
+        ])
+
+
+    def send(self):
+        """ Post fields and files to an HTTP server as multipart/form-data.
+            Return the server's response.
+        """
+        scheme, location, path, query, _ = urlparse.urlsplit(self.url)
+        assert scheme in ("http", "https"), "Unsupported scheme %r" % scheme
+
+        content_type, body = self._encode_multipart_formdata()
+        handle = getattr(httplib, scheme.upper() + "Connection")(location)
+        if self.mock_http:
+            # Don't actually send anything, print to stdout instead
+            handle.sock = parts.Bunch(
+                sendall=lambda x: sys.stdout.write(fmt.to_utf8(
+                    ''.join((c if 32 <= ord(c) < 127 or ord(c) in (8, 10) else u'\u27ea%02X\u27eb' % ord(c)) for c in x)
+                )),
+                makefile=lambda dummy, _: StringIO.StringIO("\r\n".join((
+                    "HTTP/1.0 204 NO CONTENT",
+                    "Content-Length: 0",
+                    "",
+                ))),
+                close=lambda: None,
+            )
+
+        handle.putrequest('POST', urlparse.urlunsplit(('', '', path, query, '')))
+        handle.putheader('Content-Type', content_type)
+        handle.putheader('Content-Length', str(len(body)))
+        for key, val in self.headers.items():
+            handle.putheader(key, val)
+        handle.endheaders()
+        handle.send(body)
+        #print handle.__dict__
+
+        return handle.getresponse()
+
+
+    def _encode_multipart_formdata(self):
+        """ Encode POST body.
+            Return (content_type, body) ready for httplib.HTTP instance
+        """
+        def get_content_type(filename):
+            "Helper to get MIME type."
+            return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+
+        boundary = '----------ThIs_Is_tHe_b0uNdaRY_%d$' % (time.time())
+        logical_lines = []
+        for name, value in self.fields:
+            if value is None:
+                continue
+            logical_lines.append('--' + boundary)
+            if hasattr(value, "read"):
+                filename = getattr(value, "name", str(id(value))+".dat")
+                logical_lines.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (
+                    name, 
+                    os.path.basename(filename).replace("'", '_').replace('"', '_')
+                ))
+                logical_lines.append('Content-Type: %s' % get_content_type(filename))
+                logical_lines.append('Content-Transfer-Encoding: binary')
+                value = value.read()
+            else:
+                logical_lines.append('Content-Disposition: form-data; name="%s"' % name)
+                logical_lines.append('Content-Type: text/plain; charset="UTF-8"')
+                value = fmt.to_utf8(value)
+            #logical_lines.append('Content-Length: %d' % len(value))
+            logical_lines.append('')
+            logical_lines.append(value)
+        logical_lines.append('--' + boundary + '--')
+        logical_lines.append('')
+
+        body = '\r\n'.join(logical_lines)
+        content_type = 'multipart/form-data; boundary=%s' % boundary
+        return content_type, body
---
+++ src/pyrobase/webservice/imgur.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=I0011
+""" imgur image hosting.
+
+    Copyright (c) 2012 The PyroScope Project <pyroscope.project@gmail.com>
+"""
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+from __future__ import with_statement
+
+import os
+import sys
+import socket
+import hashlib
+import httplib
+import logging
+from contextlib import closing
+
+from pyrobase import parts, pyutil, logutil, fmt
+from pyrobase.io import http
+
+json = pyutil.require_json()
+LOG = logging.getLogger(__name__)
+
+
+UploadError = (socket.error, httplib.HTTPException)
+
+
+class ImgurUploader(object): # pylint: disable=R0903
+    """ Upload an image to "imgur.com".
+
+        Sample code::
+            imgur = ImgurUploader()
+            image = imgur.upload("favicon.jpg")
+            # OR: image = imgur.upload(open("favicon.jpg", "rb"))
+            # OR: image = imgur.upload(open("favicon.jpg", "rb").read())
+            # OR: image = imgur.upload("http://i.imgur.com/5EuUx.jpg")
+            print image.links.original
+    """
+    UPLOAD_URL = "http://api.imgur.com/2/upload.json"
+
+
+    def __init__(self, api_key=None, mock_http=False):
+        """ Initialize upload parameters. 
+        
+            @param api_key: the API key (optionally taken from IMGUR_APIKEY environment variable).
+        """
+        self.api_key = api_key or os.environ.get("IMGUR_APIKEY")
+        self.mock_http = mock_http
+
+
+    def upload(self, image, name=None):
+        """ Upload the given image, which can be a http[s] URL, a path to an existing file, 
+            binary image data, or an open file handle.
+        """
+        assert self.api_key, "imgur API key is not set! Export the IMGUR_APIKEY environment variable..."
+
+        # Prepare image
+        try:
+            image_data = (image + '')
+        except (TypeError, ValueError):
+            assert hasattr(image, "read"), "Image is neither a string nor an open file handle"
+            image_type = "file"
+            image_data = image
+            image_repr = repr(image)
+        else:
+            if image.startswith("http:") or image.startswith("https:"):
+                image_type = "url"
+                image_data = image
+                image_repr = image
+            elif all(ord(i) >= 32 for i in image) and os.path.exists(image):
+                image_type = "file"
+                image_data = open(image, "rb")
+                image_repr = "file:" + image
+            else:
+                image_type = "base64"
+                image_data = image_data.encode(image_type)
+                image_repr = "<binary data>"
+            
+        # See http://api.imgur.com/resources_anon#upload
+        fields = [
+            ("key",     self.api_key),
+            ("type",    image_type),
+            ("image",   image_data),
+            ("name",    name or hashlib.md5(str(image)).hexdigest()),
+        ]
+        handle = http.HttpPost(self.UPLOAD_URL, fields, mock_http=self.mock_http)
+
+        response = handle.send()
+        LOG.debug("Image %s uploaded with result %d %s" % (image_repr, response.status, response.reason))
+        body = response.read()
+        LOG.debug("Response size: %d" % len(body))
+        LOG.debug("Response headers:\n  %s" % "\n  ".join([
+            "%s: %s" % item for item in response.getheaders()
+        ]))
+
+        try:
+            result = json.loads(body)
+        except json.decoder.JSONDecodeError, exc:
+            raise httplib.HTTPException("Bad JSON data from imgur upload [%s]: %s" % (exc, logutil.shorten(body)))
+        
+        if "error" in result: 
+            raise httplib.HTTPException("Error response from imgur.com: %(message)s" % result["error"], result)
+
+        return parts.Bunch([(key, parts.Bunch(val))
+            for key, val in result["upload"].items()
+        ])
+
+
+def fake_upload_from_url(url):
+    """ Return a 'fake' upload data record, so that upload errors 
+        can be mitigated by using an original / alternative URL. 
+    """
+    return parts.Bunch(
+        image=parts.Bunch(
+            animated='false', bandwidth=0, caption=None, views=0, deletehash=None, hash=None,  
+            name=(url.rsplit('/', 1) + [url])[1], title=None, type='image/*', width=0, height=0, size=0,
+            datetime=fmt.iso_datetime(), 
+        ), 
+        links=parts.Bunch(
+            delete_page=None, imgur_page=None, 
+            original=url, large_thumbnail=url, small_square=url,
+        ))
+
+
+def copy_image_from_url(url, cache_dir=None):
+    """ Copy image from given URL and return upload metadata.
+    """
+    json_path = None
+    if cache_dir:
+        json_path = os.path.join(cache_dir, "cached-img-%s.json" % hashlib.sha1(url).hexdigest())
+        if os.path.exists(json_path):
+            LOG.info("Fetching '%s' from cache..." % (url))
+            try:
+                with closing(open(json_path, "r")) as handle:
+                    img_data = json.load(handle)
+
+                return parts.Bunch([(key, parts.Bunch(val))
+                    for key, val in img_data.items() # BOGUS pylint: disable=E1103
+                ])
+            except (EnvironmentError, json.JSONDecodeError), exc:
+                LOG.warn("Problem reading cached data from '%s', ignoring cache... (%s)" % (json_path, exc))
+
+    LOG.info("Copying '%s'..." % (url))
+    img_data = ImgurUploader().upload(url)
+
+    if json_path:
+        with closing(open(json_path, "w")) as handle:
+            json.dump(img_data, handle)
+
+    return img_data
+
+
+def _main():
+    """ Command line interface for testing.
+    """
+    import pprint, tempfile
+
+    try:
+        image = sys.argv[1]
+    except IndexError:
+        print("Usage: python -m pyrobase.webservice.imgur <url>")
+    except UploadError, exc:
+        print("Upload error. %s" % exc)
+    else:
+        pprint.pprint(copy_image_from_url(image, cache_dir=tempfile.gettempdir()))
+
+
+# When called directly, e.g. via
+#   python -m pyrobase.webservice.imgur http://i.imgur.com/5EuUx.jpg
+if __name__ == "__main__":
+    _main()
