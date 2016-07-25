46f8cf325199917f7a50ebc6ce785d3ca6ed79be
pyroscope <pyroscope.project@gmail.com>
2016-07-25 22:56:53 +0200

    improved bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,15 @@
 # Set up project
-test -x ./bin/python || ${VIRUALENV:-/usr/bin/virtualenv} .
-. bin/activate
+SCRIPTNAME="$0"
+test "$SCRIPTNAME" != "-bash" -a "$SCRIPTNAME" != "-/bin/bash" || SCRIPTNAME="${BASH_SOURCE[0]}"
+
+deactivate 2>/dev/null || true
+pyvenv=.pyvenv/$(basename $(builtin cd $(dirname "$SCRIPTNAME") && pwd))
+test -x $pyvenv/bin/python || ${VIRUALENV:-/usr/bin/virtualenv} --no-site-packages $pyvenv
+. $pyvenv/bin/activate
+for basepkg in pip setuptools wheel; do
+    pip install -U $basepkg
+done
+python -c "import distribute" 2>/dev/null || pip uninstall -y distribute
 pip install -r requirements-dev.txt
 paver generate_setup
 paver minilib
