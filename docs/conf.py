4da7774806b029692670e601d569dfcc0bc889be
pyroscope <pyroscope.project@gmail.com>
2020-02-23 13:00:25 +0100

    docs: proper project lifetime

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pyrobase'
-copyright = u'2015 - 2018, PyroScope Project'
+copyright = u'2009 - 2020, PyroScope Project'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
